0bb34627541f429326990c36af0e70b0d2d22bac
Emilian Zawrotny <synnek1337@protonmail.com>
2025-04-16 13:57:08 +0200

    Code structure: move MULTIBOOT constants into header file

---
+++ kernel/arch/i386/boot/boot.asm
@@ -0,0 +1,22 @@
+%include 'multiboot.inc'
+
+HEADER_LENGTH equ header_end - header_start
+
+section .multiboot
+align 8
+header_start:
+    dd MULTIBOOT_MAGIC_BYTES
+    dd MULTIBOOT_ARCH_I386
+    dd HEADER_LENGTH
+    dd -(MULTIBOOT_MAGIC_BYTES + MULTIBOOT_ARCH_I386 + HEADER_LENGTH)
+    dd MULTIBOOT_TAG_TYPE_END
+header_end:
+
+section .text
+global _start
+_start:
+    cli
+    extern kmain
+    call kmain
+.hang:  hlt
+    jmp .hang
