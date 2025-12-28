dd0ae4535934f2849cc8349736b859f04fa8734c
Emilian Zawrotny <emilian@zawrotny.pl>
2025-12-28 22:43:24 +0100

    Fix PS/2 polling

--- kernel/arch/x86/include/ps2.h
+++ kernel/arch/x86/include/ps2.h
@@ -3,40 +3,14 @@
 #include <port.h>
 #include <stdio.h>
 
-#define PS2_STATUS_REGISTER 0x60
-#define PS2_COMMAND_REGISTER 0x64
-#define PS2_DATA_PORT 0x60
-
 
 static inline uint8_t ps2_read_status() {
-  return inb(PS2_STATUS_REGISTER);
+  return inb(0x64);
 }
 
 static inline uint8_t ps2_read_data() {
-  for (int i = 0; i<1024; ++i) {
+  while (1) {
     if (ps2_read_status() & 1)
-      return inb(PS2_DATA_PORT);
+      return inb(0x60);
   }
-  printf("[ps2] reading data timed out.\n");
-  return 0;
-}
-
-static inline void ps2_write_cmd(uint8_t cmd) {
-  for (int i = 0; i<1024; ++i) {
-    if (!(ps2_read_status() & 0b10)) {
-      outb(PS2_COMMAND_REGISTER, cmd);
-      return;
-    }
-  }
-  printf("[ps2] sending %X command failed due to time out.\n", cmd);
-}
-
-static inline void ps2_write_data(uint8_t data) {
-  for (int i = 0; i<1024; ++i) {
-    if (!(ps2_read_status() & 0b10)) {
-      outb(PS2_DATA_PORT, data);
-      return;
-    }
-  }
-  printf("[ps2] sending %X data failed due to time out.\n", data);
 }
--- kernel/kernel/main.c
+++ kernel/kernel/main.c
@@ -1,8 +1,12 @@
 #include <kernel/tty.h>
 #include <cpuid.h>
 #include <stdio.h>
+#include <ps2.h>
 
 void kmain() {
   puts("[+] System fully loaded.\n");
   printf("CR0: %X\n", get_cr0());
+  while (1) {
+    printf("%X", ps2_read_data());
+  }
 }
