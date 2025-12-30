5381ca9f8f59ac4d5bccf6eb781eb366d2fcce40
Emilian Zawrotny <emilian@zawrotny.pl>
2025-12-30 22:52:23 +0100

    [shell] Add clear and lscpu commands

--- kernel/shell/shell.c
+++ kernel/shell/shell.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <kernel/tty.h>
+#include <cpuid.h>
 
 extern uint64_t ticks;
 
@@ -10,6 +12,13 @@ void uptime();
 void shell_exec(const char* cmd) {
   if (strcmp(cmd, "uptime") == 0)
     uptime();
+  else if (strcmp(cmd, "clear") == 0)
+    tty_clear();
+  else if (strcmp(cmd, "lscpu") == 0) {
+    char buf[17];
+    cpuid_get_vendor(buf);
+    printf("%s\n", buf);
+  }
   else
     printf("Unrecognized command: %s\n", cmd);
 }
