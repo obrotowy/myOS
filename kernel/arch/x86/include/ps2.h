027f96203dd7924a704f8e710eb5371d0ec36f22
Emilian Zawrotny <emilian@zawrotny.pl>
2026-01-01 17:54:50 +0100

    [x86/ps2] Get rid of magic numbers from code

--- kernel/arch/x86/include/ps2.h
+++ kernel/arch/x86/include/ps2.h
@@ -3,14 +3,18 @@
 #include <port.h>
 #include <stdio.h>
 
+#define PS2_STATUS_PORT 0x64
+#define PS2_COMMAND_PORT 0x64
+#define PS2_DATA_PORT 0x60
+#define PS2_OUTPUT_BUFFER_STATUS_BIT 1
 
 static inline uint8_t ps2_read_status() {
-  return inb(0x64);
+  return inb(PS2_STATUS_PORT);
 }
 
 static inline uint8_t ps2_read_data() {
   while (1) {
-    if (ps2_read_status() & 1)
-      return inb(0x60);
+    if (ps2_read_status() & PS2_OUTPUT_BUFFER_STATUS_BIT)
+      return inb(PS2_DATA_PORT);
   }
 }
