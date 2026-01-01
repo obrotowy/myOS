8b51adb255d99e9da22dc8e305e4f0cf436be511
Emilian Zawrotny <emilian@zawrotny.pl>
2026-01-01 18:14:20 +0100

    [x86/ps2] provide function for sending to command and data registers

--- kernel/arch/x86/include/ps2.h
+++ kernel/arch/x86/include/ps2.h
@@ -7,6 +7,7 @@
 #define PS2_COMMAND_PORT 0x64
 #define PS2_DATA_PORT 0x60
 #define PS2_OUTPUT_BUFFER_STATUS_BIT 1
+#define PS2_INPUT_BUFFER_STATUS_BIT 2
 
 static inline uint8_t ps2_read_status() {
   return inb(PS2_STATUS_PORT);
@@ -16,5 +17,23 @@ static inline uint8_t ps2_read_data() {
   while (1) {
     if (ps2_read_status() & PS2_OUTPUT_BUFFER_STATUS_BIT)
       return inb(PS2_DATA_PORT);
+  }
+}
+
+static inline void ps2_send_data(uint8_t data) {
+  while (1) {
+    if (!(ps2_read_status() & PS2_INPUT_BUFFER_STATUS_BIT)) {
+      outb(PS2_DATA_PORT, data);
+      return;
+    }
+  }
+}
+
+static inline void ps2_send_command(uint8_t cmd) {
+  while (1) {
+    if (!(ps2_read_status() & PS2_INPUT_BUFFER_STATUS_BIT)) {
+      outb(PS2_COMMAND_PORT, cmd);
+      return;
+    }
   }
 }
